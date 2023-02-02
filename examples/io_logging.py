1a42a2cdbebc830030bb31567f7a9ec9c4f899b9
Tamika Nomara <taminomara@gmail.com>
2023-02-02 13:10:55 +0100

    Rename examples

---
+++ examples/io_logging.py
@@ -0,0 +1,14 @@
+import yuio.io
+
+if __name__ == '__main__':
+    yuio.io.setup(level=yuio.io.DEBUG)
+
+    yuio.io.info('<c:heading>Log level colors:</c>')
+    yuio.io.debug('  Debug message is gray.')
+    yuio.io.info('  Info message is default color.')
+    yuio.io.warning('  Warning message is yellow.')
+    yuio.io.error('  Error message is red.')
+    yuio.io.critical('  Critical message is SCARY RED. Boo!')
+    yuio.io.info('<c:heading>Color tags:</c>')
+    yuio.io.info('  This text is <c:red,bold>red!</c>')
+    yuio.io.warning('  File <c:code>example.txt</c> does not exist.')
