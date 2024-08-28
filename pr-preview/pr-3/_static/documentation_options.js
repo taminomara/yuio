934d90cfc8976273a739e9b9114c65c029977668
taminomara <taminomara@users.noreply.github.com>
2024-08-28 20:02:36 +0000

    Deploy preview for PR 3 🛫

--- pr-preview/pr-3/_static/documentation_options.js
+++ pr-preview/pr-3/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gf68526f',
+    VERSION: '0.1.dev1+g0a32d2a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
