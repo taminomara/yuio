69199ff8bdbf7b4d192fe876615099262ad8d2d0
Tamika Nomara <taminomara@gmail.com>
2024-08-31 19:23:33 +0400

    Multiselect demo

---
+++ examples/prompt_multiselect.py
@@ -0,0 +1,20 @@
+import enum
+from typing import Set
+
+import yuio.io
+import yuio.parse
+
+
+class Console(enum.Enum):
+    GameBoy = "GameBoy"
+    Wii = "Wii"
+    PlayStation2 = "PlayStation2"
+    PlayStationPortable = "PlayStationPortable"
+    XboxOne = "XboxOne"
+    Genesis = "MegaDrive/Genesis"
+    Atari2600 = "Atari2600"
+
+
+if __name__ == "__main__":
+    consoles = yuio.io.ask[Set[Console]]("Which consoles do you own?")
+    yuio.io.info("You own `%s`", ", ".join(map(str, consoles)) or "...nothing?")
