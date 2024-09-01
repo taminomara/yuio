57052c35a75d3b74d6c5488928579e2e7d66ece0
Tamika Nomara <taminomara@gmail.com>
2024-09-01 22:52:10 +0400

    Improve testing experience

--- conftest.py
+++ conftest.py
@@ -2,7 +2,6 @@ from sybil import Sybil
 from sybil.parsers.codeblock import PythonCodeBlockParser
 from sybil.parsers.doctest import DocTestParser
 
-
 pytest_collect_file = Sybil(
     parsers=[
         DocTestParser(),
